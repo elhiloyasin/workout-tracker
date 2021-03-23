91c9a29461b74261f5836ac5b23678970a919c63
Yasin Elhilo <elhiloyasin@gmail.com>
2021-03-23 15:52:57 -0400

    Messed up and created a new repo to put my home in

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# workout-tracker
+# Fitness-Tracker
+
+
+
+### Description
+>The purpose of this application is to track all exercises that the user inputs. The user has the ability to add on to their previous exercises or input new excercises they make.
+
+### Technologies Used
+
+>- Morgan
+>- Mongo Database
+>- Express
+>- Mongoose
+>- Node.js
+>- robo 3T
+
+
+
+## REPOLINK  
+
+>[Fitness-Tracker](https://github.com/elhiloyasin/fitnessTracker)
+
+
+
---
+++ routes/api.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req , res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    {$push:{exercises:req.body}}
+  )
+    .then(dbworkouts=> {
+      res.json(dbworkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbworkouts => {
+      res.json(dbworkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+    .then(dbworkouts => {
+      res.json(dbworkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
